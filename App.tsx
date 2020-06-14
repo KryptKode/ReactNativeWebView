910c7807eb970c2ab17536ede1c79536da2b7a37
kryptkode <paulakhamiogu@gmail.com>
2020-06-14 22:15:25 +0100

    Add webiew

--- App.tsx
+++ App.tsx
@@ -1,19 +1,8 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { WebView } from "react-native-webview";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <WebView source={{ uri: "https://expo.io" }} style={{ marginTop: 20 }} />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
